73d4ea8a75583bd7122717c3545edce7f5441faa
mahdi golamozadeh <mahdi.paydar.smart@gmail.com>
2024-01-17 21:31:38 +0330

    update 1

--- next.config.js
+++ next.config.js
@@ -1,17 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ["wings-cms.storage.iran.liara.space","images.wallpapersden.com", "www.canon.com.au","webwinkel.groenlinks.nl"],
-
-    },
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'wings-cms.storage.iran.liara.space',
-        port: '3000',
-        pathname: '/**',
-      },
+  basePath: "/wings-cms",
+  assetPrefix: "/wings-cms/",
+  images: {
+    domains: [
+      "wings-cms.storage.iran.liara.space",
+      "images.wallpapersden.com",
+      "www.canon.com.au",
+      "webwinkel.groenlinks.nl",
     ],
-  }
-  
-  module.exports = nextConfig
+  },
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "wings-cms.storage.iran.liara.space",
+      port: "3000",
+      pathname: "/**",
+    },
+  ],
+};
+
+module.exports = nextConfig;
